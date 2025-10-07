316cfacc6a174d18632ef01af04cf6cbc8fc3ef5
Alex Shahbazi <materialsmoke@gmail.com>
2025-10-07 16:18:02 +0200

    fix update btn

--- resources/views/admin/product/edit.blade.php
+++ resources/views/admin/product/edit.blade.php
@@ -11,7 +11,7 @@
             @include('admin.product.form')
 
 
-            <button type="submit" class="btn btn-primary">Update</button>
+            <button type="submit" class="btn btn-primary" onclick="clickSaveAll(event)">Update</button>
         </form>
     </div>
 
@@ -21,3 +21,18 @@
     @include('admin.partials.add-widget-form')
 
 @endsection
+
+<script>
+function clickSaveAll(event) {
+    event.preventDefault(); // Prevent the default form submission
+    
+    // Find and click the save-all button
+    const saveAllButton = document.getElementById('save-all');
+    if (saveAllButton) {
+        saveAllButton.click();
+    }
+    
+    // Then submit the form
+    event.target.closest('form').submit();
+}
+</script>
