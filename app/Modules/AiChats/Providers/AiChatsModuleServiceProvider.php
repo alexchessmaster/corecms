3e759720c57f03b09ed8a127012a06920f4242df
Alex <materialsmoke@gmail.com>
2026-02-06 01:22:56 +0100

    fix aichats

--- app/Modules/AiChats/Providers/AiChatsModuleServiceProvider.php
+++ app/Modules/AiChats/Providers/AiChatsModuleServiceProvider.php
@@ -4,7 +4,7 @@ namespace App\Modules\AiChats\Providers;
 
 use Illuminate\Support\ServiceProvider;
 
-class AiChatModuleServiceProvider extends ServiceProvider
+class AiChatsModuleServiceProvider extends ServiceProvider
 {
     /**
      * Register services.
